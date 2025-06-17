6b03b5ac52483d01ae8a5cadb8f5ae99c54b62c4
Cezary Jagiello <Cezary.Jagiello>
2025-06-17 15:06:41 +0200

    Add comment/description fot entity service.

--- src/Service/EntityService.php
+++ src/Service/EntityService.php
@@ -4,6 +4,12 @@ namespace App\Service;
 
 class EntityService
 {
+    /**
+     * Map String to systemName (CamelCase)
+     *
+     * @param string $string
+     * @return string
+     */
     public function toSystemName(string $string): string
     {
         $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
